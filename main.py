26a8f374ea7c1ad38943d652d78636997c6f263f
Clay Zhong <zjclay@gmail.com>
2014-08-08 12:01:51 +0900

    Updated index page, added snapshots.

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 @app.route('/')
-def hello():
+def index():
     """Return a friendly HTTP greeting."""
-    return 'Hello Crazy Quiz!'
-
+    return render_template('index.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
