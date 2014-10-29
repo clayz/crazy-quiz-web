2e3fa9222a3741056dab2162ff9ecca28f4a6706
Clay Zhong <zjclay@gmail.com>
2014-10-29 18:42:28 +0900

    Upgrade version number.

--- src/api/user_api.py
+++ src/api/user_api.py
@@ -1,3 +1,4 @@
+from google.appengine.ext import ndb
 from flask import Blueprint, request
 from constants import DEFAULT_GEM, DEFAULT_COIN, Device, UserStatus, APIStatus
 from utilities import response, get_form
@@ -14,14 +15,16 @@ def startup():
     from main import app
 
     form = get_form(StartupForm(request.form))
-    uuid, device, version = form.uuid.data, form.device.data, form.version.data,
-    app.logger.info("App startup, UUID: %s" % uuid)
+    uuid, name, device, version = form.uuid.data, form.name.data, form.device.data, form.version.data
 
-    user = User.get_by_id(uuid)
-    if not user:
-        user = User(id=uuid, device=Device.lookup_by_number(device))
+    app.logger.info("App startup, uuid: %s" % uuid)
+    user = User.get_by_id(uuid) or create_user(uuid, name, Device.lookup_by_number(device))
+
+    if user.status == UserStatus.INACTIVE and name != '':
+        user.name = name
+        user.status = UserStatus.ACTIVE
         user.put()
-        app.logger.info("Created new user: %s" % user)
+        app.logger.info("Active user: %s" % user)
 
     startup_history = StartupHistory(parent=user.key, version=version, ip=request.remote_addr)
     startup_history.put()
@@ -35,33 +38,50 @@ def register():
 
     form = get_form(RegisterForm(request.form))
     user = User.get(form.uuid.data)
+
     if user.status == UserStatus.ACTIVE:
         raise DataError(APIStatus.DATA_SAVE_FAILED, 'User already registered: %s' % user)
 
-    app.logger.info("User register, UUID: %s" % user.key)
+    app.logger.info("User register, uuid: %s" % user.key)
     user.name = form.name.data
     user.status = UserStatus.ACTIVE
     user.put()
-
-    currency = Currency(parent=user.key, gem=DEFAULT_GEM, coin=DEFAULT_COIN)
-    currency.put()
 
     return response()
 
 
 @user_api.route('/notification/', methods=['POST'])
 def register_notification():
+    from main import app
+
     form = get_form(PushNotificationForm(request.form))
     user, push_token = User.get(form.uuid.data), form.push_token.data
 
     if user.push_token != push_token:
+        app.logger.info('Update push token, uuid: %s, push_token: %s' % user.key, push_token)
         user.push_token = push_token
         user.put()
 
     return response()
 
 
+@ndb.transactional()
+def create_user(uuid, name, device):
+    from main import app
+
+    user = User(id=uuid, name=name, device=device)
+    user.put()
+    app.logger.info("Created new user: %s" % user)
+
+    currency = Currency(parent=user.key, gem=DEFAULT_GEM, coin=DEFAULT_COIN)
+    currency.put()
+    app.logger.debug("Created currency: %s" % currency)
+
+    return user
+
+
 class StartupForm(BaseForm):
+    name = StringField('name', [validators.optional(), validators.length(min=1, max=8)])
     version = StringField('version', [validators.length(min=3, max=5)])
     device = IntegerField('device', [validators.number_range(min=0, max=3)])
 
